53c3ffc1d378338c87890c7f4d5f4aebc802075d
Shristi Koju <kojushristi4@gmail.com>
2024-11-09 21:13:00 +0545

    modal project added via npm

---
+++ modal-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{ title }}</h1>
+  <Modal/>
+</template>
+
+<script>
+import Modal from './components/ModalProject.vue'
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App'
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  }
+}
+</script>
+
+
+<style>
+  h1{
+    color: black;
+  }
+</style>
