2074ae32e17fbfd9ba006d6b2865d8dc3389a12d
Shristi Koju <kojushristi4@gmail.com>
2024-11-15 14:41:05 +0545

    reaction-timer

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play!</button>
+  <TimerBlock v-if="isPlaying" :delay="delay"/>
+</template>
+
+<script>
+import TimerBlock from "./components/TimerBlock.vue";
+
+export default {
+  name: "App",
+  components: { TimerBlock },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
